5a302fb9d39a903509263dcdfe108e5f0b68d96e
oveddan <stangogh@gmail.com>
2014-05-13 00:46:39 -0400

    added recursive runner example that can work with a sequence of yields

--- test/generators_real_examples.js
+++ test/generators_real_examples.js
@@ -39,4 +39,65 @@ describe('real generator examples', function(){
       });
     });
   });
+  it('can call the asynchronous methods recursively', function(done){
+    var asynchronousUrlShortener = function(url, callback) {
+      var shortenedUrls = {
+        'http://www.sitea.com' : 'http://m.s.l/5',
+        'http://www.siteb.com' : 'http://m.s.l/2',
+        'http://www.sitec.com' : 'http://m.s.l/9',
+        'http://www.sited.com' : 'http://m.s.l/2',
+        'http://www.sitef.com' : 'http://m.s.l/4',
+      }
+
+      setTimeout(function(){
+        var shortenedUrl = shortenedUrls[url];
+        callback(null, shortenedUrl);
+      }, 100);
+    }
+
+    var urlShortenerThunk = function(url) {
+      return function callbackForShortenUrl(callback) {
+        asynchronousUrlShortener(url, callback);
+      }
+    };
+
+    var generator = (function *(done){
+      var urlsToShorten = [
+        'http://www.siteb.com',
+        'http://www.sited.com',
+        'http://www.sitef.com',
+        'http://www.sitea.com'
+      ];
+
+      var shortenedUrls = [];
+
+      for(var i = 0; i < urlsToShorten.length; i++){
+        var shortenedUrl = yield urlShortenerThunk(urlsToShorten[i]);
+        shortenedUrls.push(shortenedUrl);
+      };
+
+      return shortenedUrls;
+    })();
+
+    var recursiveGeneratorRunner = function(gen, callback) {
+      next()
+ 
+      function next (er, value) { 
+        if (er) return gen.throw(er);
+        var continuable = gen.next(value);
+        if (continuable.done) {
+          callback(null, continuable.value);
+        } else {
+          var cbFn = continuable.value;
+          cbFn(next);
+        }
+      }
+    };
+
+
+    recursiveGeneratorRunner(generator, function(err, result){
+      expect(result).to.eql(['http://m.s.l/2', 'http://m.s.l/2', 'http://m.s.l/4', 'http://m.s.l/5']);
+      done();
+    });  
+  });
 });
